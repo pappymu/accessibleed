06622d06ea2544faff24705398f30979602214d1
Joven Soh <34702990+JovenSoh@users.noreply.github.com>
2022-08-13 10:26:38 +0800

    Updated landing page 2

--- src/Components/Landing2.jsx
+++ src/Components/Landing2.jsx
@@ -1,6 +1,5 @@
 import { Box, Stack, Button, Divider } from '@mui/material/';
 import "./Landing2.css";
-import graduateImage from "./../assets/graduate.jpg"
 
 const LandingOne = () => {
 
@@ -9,25 +8,18 @@ const LandingOne = () => {
             class='Landing2'>
 
             <Stack
-                sx={{
-                    alignItems: 'center',
-                }}
                 direction="row">
 
-                <Box className='glass' sx={{ padding: 5 }}>
-                    <h1>Empowering youths through free education</h1>
+                <Box class='glass' sx={{ padding: 5 }}>
+                    <h1>EMPOWERING YOUTHS THROUGH <br /> FREE EDUCATION</h1>
                     <p>We're a non-profit organisation with a mission to provide tutoring services and access to learning resources for under-privileged students.</p>
                     <Box className="buttonsInnerDiv">
-                        <Box elevation={12} className="buttonPaperStyle variant1" sx={{ mr: "2vw"}}>
-                            <span className="iconStyle">👨‍🎓</span>
+                        <Box elevation={12} className="buttonPaperStyle variant1" sx={{ mr: "1vw"}}>
                             <span className="iconTextStyle">Find Tutors</span>
                         </Box>
 
-                        <Divider orientation="vertical" style={{ borderRightWidth: "5px", height: "auto" }} />
-
-                        <Box elevation={12} className="buttonPaperStyle variant2" sx={{ marginLeft: "2vw" }}>
-                            <span className="iconStyle">👩‍🏫</span>
-                            <span className="iconTextStyle">Volunteer With Us</span>
+                        <Box elevation={12} className="buttonPaperStyle variant2" sx={{ marginLeft: "1vw" }}>
+                            <span className="iconTextStyle2">Volunteer With Us</span>
                         </Box>
                     </Box>
                 </Box>
